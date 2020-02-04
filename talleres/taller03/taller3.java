bf3581ebc434391ae094882d366c5831479d917b
Nelson A. Barrios Jiménez <47148094+nabarriosj@users.noreply.github.com>
2020-02-04 11:06:45 -0500

    Update and rename archivo.java to taller3.java

---
+++ talleres/taller03/taller3.java
@@ -0,0 +1,21 @@
+* @author Nelson Barrios, Santiago Montoya
+ 
+public class Taller3 {
+
+	
+    public static void torresDeHannoi(int n) {
+	torresDeHannoiAux(n, 1, 2, 3);
+    }
+	
+    private static void torresDeHannoiAux(int n, int origen, int intermedio, int destino) {
+	if(n == 1)
+	    {   
+		System.out.println("Mover disco de "+ origen + " a "+ destino);
+	    }
+	else {
+	    torresDeHannoiAux(n - 1, origen, intermedio,destino );
+	    System.out.println(" Disco "+ n + " de "+ origen + " a "+ destino);
+	    torresDeHannoiAux(n - 1, intermedio, origen, destino);
+	}
+    }
+		
