b942d3213890116941de8604759b03fee42b1f77
smontoyat-git <60165809+smontoyat-git@users.noreply.github.com>
2020-02-09 22:40:06 -0500

    Update Recursion2.java

--- laboratorios/lab01/ejercicioEnLinea/Recursion2.java
+++ laboratorios/lab01/ejercicioEnLinea/Recursion2.java
@@ -1,5 +1,15 @@
 * @author: Santiago Montoya Tobon, Nelson Andres Barrios Jimenez
 public class Recursion2 {
-
-
-}
+   public boolean groupSum6(int start, int[] nums, int target) {
+    if (start >= nums.length) 
+      return target == 0;
+    if (nums[start] == 6)
+        return groupSum6(start + 1, nums, target - nums[start]);
+    return groupSum6(start + 1, nums, target - nums[start]) || groupSum6(start + 1, nums, target);
+  }
+  public boolean groupNoAdj(int start, int[] nums, int target) {
+    if (start >= nums.length) 
+      return target == 0;
+    return groupNoAdj(start + 2, nums, target - nums[start]) || groupNoAdj(start + 1, nums, target);
+  }
+   
