301944ac1d901e64003e9e4eeb366203bcf3c44e
smontoyat-git <60165809+smontoyat-git@users.noreply.github.com>
2020-02-29 15:40:01 -0500

    Update Array2.java

--- laboratorios/lab02/ejercicioEnLinea/Array2.java
+++ laboratorios/lab02/ejercicioEnLinea/Array2.java
@@ -1,1 +1,32 @@
+public class laboratorio2_array2 {
+  
+  //Ejercicio countEvens
+  public int countEvens(int[] nums) {
+    int contador = 0;
+      for(int i = 0; i<nums.length; i++) {
+        if(nums[i] % 2 == 0) {
+          contador += 1;
+        }
+      }
+      return contador;
+  }
+  
+  //Ejercicio centeredAverage
+  public int centeredAverage(int[] nums) {
+    int max = nums[0];
+    int min = nums[0];
+    int sum = nums[0];
+    for(int i = 1; i < nums.length; i++) {
+	    sum  += nums[i];
+	    if(nums[i] > max) { 
+        max = nums[i]; 
+      } 
+      else if(nums[i] < min) min = nums[i];
+    }
+    return (sum-max-min) / (nums.length - 2);
+  }
+  
+  //Ejercicio sum13
+  public int sum13
 
+}
