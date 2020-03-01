7ab0d12c178a2104e6b3e5c7fe87c07171f95095
smontoyat-git <60165809+smontoyat-git@users.noreply.github.com>
2020-03-01 11:57:45 -0500

    Update Array3.java

--- laboratorios/lab02/ejercicioEnLinea/Array3.java
+++ laboratorios/lab02/ejercicioEnLinea/Array3.java
@@ -1,1 +1,21 @@
+public class Array3 {
+  
+  //Ejercicio maxSpan
+  public int maxSpan(int[] nums) {
+    int maxS = 0;
+    for(int i = 0; i < nums.length; i++) {
+      for(int j = nums.length - 1; j >= i; j--) {
+        if(nums[i] == nums[j]) {
+          if((i - j +1) > max) {
+            max = i - j + 1;
+          }
+          break;
+        }
+      }
+    }
+    return max;
+  }
 
+
+
+}
