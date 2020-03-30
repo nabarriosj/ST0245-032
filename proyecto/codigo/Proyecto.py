ea84fa36854a0391b2f8c01b206af64fb50eeca0
smontoyat-git <60165809+smontoyat-git@users.noreply.github.com>
2020-03-29 19:37:52 -0500

    Update Proyecto.py

--- proyecto/codigo/Proyecto.py
+++ proyecto/codigo/Proyecto.py
@@ -1,1 +1,13 @@
+'''
+@author: SantiagoMontoyaTobon - NelsonAndresBarrios
+el siguiente codigo esta diseñado para leer un archivo con extension csv y organizarlos en un arreglo de arreglos tambien llamado matriz
+'''
+import csv        #importamos el modulo csv que cuenta con una funcion llamada reader que utilizaremos para leer los datos
 
+def Lectura_Datos(archivo):      #definimos la funcion que utilizaremos para invocar el reader
+  data = []
+  with open(archivo, encoding = 'utf-8') as datos:    #abrimos el archivo y utilizamos el encoding para codificar cada caracter y hacerlo reconocible 
+    leer = csv.reader(datos, delimiter=";")  #invocamos el metodo reader que recibe el archivo y como metodo opcional el delimitador, sacado de: https://code.tutsplus.com/es/tutorials/how-to-read-and-write-csv-files-in-python--cms-29907
+    for line in leer:
+      data.append(line)      #agregamos el item al final de la lista 
+    print(data)
