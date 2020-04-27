ce26e84c675aad71a03fda899451f5842b69d9f9
smontoyat-git <60165809+smontoyat-git@users.noreply.github.com>
2020-04-26 23:06:42 -0500

    Update Punto1.java

--- laboratorios/lab04/codigo/Punto1.java
+++ laboratorios/lab04/codigo/Punto1.java
@@ -1,25 +1,25 @@
 public class Punto1 {
 	private static Nodo1 raiz = new Nodo1("Proyecto", null);
 
-	public static void insertar(String nombre, double tamaño) {
-		Nodo1 n = new Nodo1(nombre, tamaño, raiz);
-		raiz.añadirArchivos(n);
+	public static void insertar(String nombre, double tamanio) {
+		Nodo1 n = new Nodo1(nombre, tamanio, raiz);
+		raiz.aÃ±adirArchivos(n);
 	}
 
-	public static void insertar(String directorio, String nombre, double tamaño) {
+	public static void insertar(String directorio, String nombre, double tamanio) {
 		Nodo1 nodo = insertarValor(raiz, directorio);
-		Nodo1 n = new Nodo1(nombre, tamaño, nodo);
-		nodo.añadirArchivos(n);
+		Nodo1 n = new Nodo1(nombre, tamanio, nodo);
+		nodo.aÃ±adirArchivos(n);
 	}
 
 	public static Nodo1 insertarValor(Nodo1 nodo, String nombre) {
 		if(nodo.getNombre().equals(nombre)) return nodo;
-		for(int i = 0; i < nodo.getTamañoArchivos(); i++);
+		for(int i = 0; i < nodo.getTamanioArchivos(); i++);
 			insertarValor(nodo.getArchivos(i), nombre);
 		return null;		
 	}
 
-	public static void buscar(String s, double tamaño) {
+	public static void buscar(String s, double tamanio) {
 		String [] arreglo = s.split("/");
 		buscarValor(arreglo, raiz, 0);
 	}
@@ -30,27 +30,27 @@ public class Punto1 {
 	} 
 
 	public static void buscarValor(String [] arreglo, Nodo1 nodo, int i) {
-		if(nodo.getNombre().equals(array[i])) {
-			for(int j = 0; i < nodo.getTamañoArchivos(); i++) {
+		if(nodo.getNombre().equals(arreglo[i])) {
+			for(int j = 0; i < nodo.getTamanioArchivos(); i++) {
 				System.out.println(nodo.getArchivos(i).getNombre());
 			}
 		}
 		if(i <= arreglo.length) {
-			for(int j = 0; i < nodo.getTamañoArchivos(); i++) {
+			for(int j = 0; i < nodo.getTamanioArchivos(); i++) {
 				buscarValor(arreglo, nodo, i++);
 				buscarValor(arreglo, nodo.getArchivos(i), 0);
 			}
 		}	
 	}
 	
-	public static void buscarValor(String [] arreglo, Nodo1 nodo, int i, double tamaño) {
-		if(nodo.getNombre().equals(array[i])) {
-			for(int j = 0; j < nodo.getTamañoArchivos(); j++) {
-				if(nodo.getArchivos(j).getTamaño() < tamaño) System.out.println(nodo.getArchivo(i).getNombre());                                 
+	public static void buscarValor(String [] arreglo, Nodo1 nodo, int i, double tamanio) {
+		if(nodo.getNombre().equals(arreglo[i])) {
+			for(int j = 0; j < nodo.getTamanioArchivos(); j++) {
+				if(nodo.getArchivos(j).getTamanio() < tamanio) System.out.println(nodo.getArchivo(i).getNombre());                                 
 			}
 		} else System.out.println("no such file or directory");
 		if(i <= arreglo.length) {
-			for(int j = 0; j < nodo.getTamañoArchivos(); i++) {
+			for(int j = 0; j < nodo.getTamaÃ±oArchivos(); i++) {
 				buscarValor(arreglo, nodo, i++);
 				buscarValor(arreglo, nodo.getArchivos(i), 0);
 			}
