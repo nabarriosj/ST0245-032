2bef9462507ab2e6b85fcf906588efec74a03a64
smontoyat-git <60165809+smontoyat-git@users.noreply.github.com>
2020-05-06 22:37:25 -0500

    Update Graph.java

--- talleres/taller12/Graph.java
+++ talleres/taller12/Graph.java
@@ -3,11 +3,9 @@ import java.util.ArrayList;
 public abstract class Graph {
     
 	protected int size;
-
 	public Graph(int vertices) {
 		size = vertices;
 	}
-
 	public abstract void addArc(int source, int destination, int weight);
         
 	public abstract ArrayList<Integer> getSuccessors(int vertex);
